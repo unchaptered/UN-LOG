95f6c0770fbdf752146e1c1c520f6182d88aac36
Unchaptered <workstation19961002@gmail.com>
2022-10-07 02:41:28 +0900

    Docs/Update ~docs/UTILS.md

--- docs/UTILS.md
+++ docs/UTILS.md
@@ -1,4 +1,5 @@
 # Utility Libraries
 
 - [x] mrm for `husky`, `lint-staged` [Open Docs](https://mrm.js.org/docs/mrm-task-lint-staged)
-- [x] prettier [Open Docs](https://github.com/sveltejs/prettier-plugin-svelte)
+- [x] prettier [Open GitHUb](https://github.com/sveltejs/prettier-plugin-svelte)
+- [x] mdsvex site [Open Docs](https://mdsvex.com/docs) [Open GitHUb](https://github.com/pngwn/MDsveX)
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,26 @@
 import staticAdapter from '@sveltejs/adapter-auto';
+
+import { mdsvex } from 'mdsvex';
 import sveltePreprocess from 'svelte-preprocess';
+
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
 
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		sveltePreprocess({
+			postcss: {
+				plugins: [autoprefixer()]
+			}
+		}),
+		mdsvex({
+			extensions: ['.md']
+		})
+	],
 	kit: {
 		adapter: staticAdapter()
 	}
